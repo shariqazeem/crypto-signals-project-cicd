075bb874cde40df626e59bf7a18809047eb3da85
shariqazeem <shariqshaukat786@gmail.com>
2023-08-08 22:18:41 +0500

    added formula for risk management calculations

--- static/js/script.js
+++ static/js/script.js
@@ -25,31 +25,27 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+    document.addEventListener("DOMContentLoaded", function () {
+        const calculateBtns = document.querySelectorAll(".calculate-btn");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const calculateBtn = document.querySelector(".calculate-btn");
-  calculateBtn.addEventListener("click", function () {
-    // Get the portfolio size, risk percentage, entry price, take profit percentage, and stop loss from the user input
-    const portfolioSize = parseFloat(document.querySelector(".portfolio-size").value);
-    const riskPercentage = parseFloat(document.querySelector(".risk-percentage").value);
-    const entryPrice = parseFloat(document.querySelector(".entry-price").value);
-    const takeProfitPercentage = parseFloat(document.querySelector(".buy-range").dataset.tp);
-    const stopLoss = parseFloat(document.querySelector(".stop-loss").textContent);
+        calculateBtns.forEach((calculateBtn) => {
+            calculateBtn.addEventListener("click", function () {
+                const card = calculateBtn.closest(".signal-card");
+                const portfolioSize = parseFloat(card.querySelector(".portfolio-size").value);
+                const riskPercentage = parseFloat(card.querySelector(".risk-percentage").value);
+                const entryPrice = parseFloat(card.querySelector(".entry-price").value);
+                const stopLossPrice = parseFloat(card.querySelector(".stop-loss").textContent);
 
-    // Calculate the total amount, number of coins to buy, amount after profit, and amount after stop loss
-    const totalAmount = portfolioSize * (riskPercentage / 100);
-    const coinsToBuy = (totalAmount / entryPrice).toFixed(2);
-    const takeProfit = parseInt(takeProfitPercentage * 100);
-    const amountAfterProfit = (totalAmount + totalAmount * takeProfit * 0.01).toFixed(2);
-    const amountAfterStopLoss = (totalAmount - totalAmount * stopLoss).toFixed(2);
+                const totalAmountAtRisk = (portfolioSize * riskPercentage) / 100;
+                const positionSize = totalAmountAtRisk / (entryPrice - stopLossPrice);
 
-    // Set the result HTML element with the calculated values
-    document.querySelector(".result").innerHTML = `
-      Number of Coins to Buy: ${coinsToBuy}<br>
-      Amount After Profit: $${amountAfterProfit}<br>
-      Amount After Stop Loss: $${amountAfterStopLoss}
-    `;
-  });
-});
+                const resultElement = card.querySelector(".result");
+                resultElement.innerHTML = `Position Size: ${positionSize.toFixed(2)}`;
+            });
+        });
+    });
+
+
+
 
 
